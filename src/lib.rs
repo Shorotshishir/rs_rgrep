81634d4ace39fbea972c7fee3f3c35861d5c8e04
Siratim Mustaquim <tanzim121@gmail.com>
2023-09-20 23:07:53 +0900

    add basic search

--- src/lib.rs
+++ src/lib.rs
@@ -19,6 +19,62 @@ impl Config {
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>>{
     let content = fs::read_to_string(config.file_path)?;
-    println!("With text \n{}", content);
+    for line in search(&config.query, &content){
+        println!("{}", line);
+    }
     return Ok(());
+}
+
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+    return results;
+}
+
+pub fn search_case_insensitive<'a>(query:&str, contents:&'a str) -> Vec<&'a str>{
+    let query = query.to_lowercase();
+    let mut results = Vec::new();
+    for line in contents.lines(){
+        if line.to_lowercase().contains(&query) {
+            results.push(line);
+        }
+    }
+    return results;
+}
+
+#[cfg(test)]
+mod tests {
+    use std::panic::resume_unwind;
+    use super::*;
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let content = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+        assert_eq!(
+            vec!["safe, fast, productive."],
+            search(query, content)
+        );
+    }
+
+    #[test]
+    fn case_sensitive(){
+        let query = "rUsT";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Trust me.";
+
+        assert_eq!(
+            vec!["Rust:", "Trust me."],
+            search_case_insensitive(query, contents)
+        );
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -3,16 +3,12 @@ use std::{env, process};
 use rs_rgrep::Config;
 
 fn main() {
-    println!("Hi ! this is rgrep!");
     let args:  Vec<String> = env::args().collect();
 
     let config = Config::build(&args).unwrap_or_else(|err|{
         println!("Problem parsing arguments {err}");
         process::exit(1);
     });
-
-    println!("Searching for {}",config.query);
-    println!("In File {}",config.file_path);
 
     if let Err(e) = rs_rgrep::run(config) {
         println!("Application Error ! {e}");
